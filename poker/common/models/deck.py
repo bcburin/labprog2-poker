9bad9b8dd1e19b21f024846369e9ea2b72c7599c
bcburin <cburin.bruno@gmail.com>
2022-11-01 13:12:26 -0300

    Added information state to game_state

--- poker/common/models/deck.py
+++ poker/common/models/deck.py
@@ -1,4 +1,7 @@
+from __future__ import annotations
+
 from typing import Iterable
+import random
 
 from poker.common.enums.rank import CardRank
 from poker.common.enums.suit import CardSuit
@@ -7,8 +10,23 @@ from poker.common.models.card import Card
 
 class Deck:
 
+    def __init__(self, cards: list[Card], shuffle: bool = True):
+        self._cards = random.shuffle(cards) if shuffle else cards
+
+    @property
+    def cards(self):
+        return self._cards
+
+    def shuffled(self) -> Deck:
+        return Deck(cards=self._cards, shuffle=True)
+
     @staticmethod
     def iterate_full_deck() -> Iterable[Card]:
         for suit in CardSuit.suits():
             for rank in CardRank.ranks():
                 yield Card(rank=rank, suit=suit)
+
+    @staticmethod
+    def get_full_deck(shuffle: bool = True) -> Deck:
+        cards = list(Deck.iterate_full_deck())
+        return Deck(cards=cards, shuffle=shuffle)
--- poker/common/models/game_state.py
+++ poker/common/models/game_state.py
@@ -1,6 +1,12 @@
-from dataclasses import dataclass
+from __future__ import annotations
+
+from dataclasses import dataclass, field
+from typing import Any
 
 from poker.common.enums.street import Street
+from poker.common.models.card import Card
+from poker.common.models.deck import Deck
+from poker.common.models.game import Game
 from poker.common.models.player import Player
 from poker.common.models.player_state import PlayerState
 
@@ -8,11 +14,43 @@ from poker.common.models.player_state import PlayerState
 @dataclass(frozen=True)
 class GameState:
 
+    game: Game
     player_states: dict[Player, PlayerState]
-    pot: int
-    bet: int
-    street: Street
-    player_can_check: bool
+    player_order: list[Player]
+    current_player: Player
+    pot: int = 0
+    bet: int = 0
+    street: Street = Street.PRE_FLOP
+    start_of_street: bool = True
+    player_can_check: bool = False
+    deck: Deck = Deck.get_full_deck()
+    turned_cards: list[Card] = field(default_factory=list)
+
+    @property
+    def last_player(self) -> Player | None:
+        if self.start_of_street:
+            return None
+        for i, player in enumerate(self.player_order):
+            if player == self.current_player:
+                return self.player_order[i-1] if i != 0 else self.player_order[-1]
+
+    @property
+    def next_player(self) -> Player:
+        current_index = self.player_order.index(self.current_player)
+        return self.player_order[(current_index + 1) % len(self.player_order)]
+
+    @property
+    def players(self) -> list[Player]:
+        return self.player_order
 
     def get_state_of(self, player: Player) -> PlayerState:
         return self.player_states[player]
+
+    def update_deck(self, cards: list[Card], shuffle: bool = True) -> GameState:
+        return self.update_state('deck', Deck(cards=cards, shuffle=shuffle))
+
+    def update_turned_cards(self, cards: list[Card]) -> GameState:
+        return self.update_state('turned_cards', cards)
+
+    def update_state(self, attr: str, val: Any) -> GameState:
+        return GameState(**{**self.__dict__, attr: val})
