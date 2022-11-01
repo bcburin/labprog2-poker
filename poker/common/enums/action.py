248eca23b1e0d63d5c15bdb0355640848f0c2dca
bcburin <cburin.bruno@gmail.com>
2022-10-31 23:34:34 -0300

    Enhanced Action enum

--- poker/common/enums/action.py
+++ poker/common/enums/action.py
@@ -1,10 +1,21 @@
+from __future__ import annotations
+
 from enum import Enum
 
 
 class Action(Enum):
 
-    CALL = 1
-    RAISE = 2
-    FOLD = 3
-    CHECK = 4
-    BET = 5
+    CALL = 'Call'
+    RAISE = 'Raise'
+    FOLD = 'Fold'
+    CHECK = 'Check'
+    BET = 'Bet'
+
+    @classmethod
+    def get_actions(cls, is_preflop: bool = False, is_blind: bool = False, can_check: bool = False) -> list[Action]:
+        if is_preflop and is_blind:
+            return [Action.BET]
+        actions = [cls.CALL, cls.RAISE, cls.FOLD]
+        if can_check:
+            actions.append(cls.CHECK)
+        return actions
